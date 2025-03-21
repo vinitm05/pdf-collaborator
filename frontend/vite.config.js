420e456d747c0f87d9212cfb12cf13265a479b04
Vinit Mittal <vinitmittal14@gmail.com>
2025-03-21 12:28:50 +0530

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
-import { viteWorker } from "vite-plugin-worker";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteWorker()],
+  plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
       output: {
